76eb8809ea88ae816962cbd92d7e60390a0decc1
Hitesh <hkhiteshkumar66@gmail.com>
2023-08-13 10:49:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </>
+    );
+}
+
+export default App;
